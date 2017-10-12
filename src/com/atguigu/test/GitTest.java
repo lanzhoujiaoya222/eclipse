9d5f9fdec1e94f74c071d260705b5c2f6978338e
mwm_golbal <mwm@136.com>
2017-10-12 10:56:13 +0800

    second

--- src/com/atguigu/test/GitTest.java
+++ src/com/atguigu/test/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("test git commit");
 		System.out.println("aaaaaaaaaaaaaa");
-		System.out.println("aaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaasdfsfaa");
 	}
 }
