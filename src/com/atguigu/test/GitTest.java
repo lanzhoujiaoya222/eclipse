d82ece0050fe48dd18e500be89f6f706c1d319ae
mwm_golbal <mwm@136.com>
2017-10-12 10:10:39 +0800

    my second commit

--- src/com/atguigu/test/GitTest.java
+++ src/com/atguigu/test/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("test git commit");
+		System.out.println("aaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaa");
 	}
 }
